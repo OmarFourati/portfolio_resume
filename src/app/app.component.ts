4966620a55134b22e4f72e6cd8c7a7eff43262e1
Omar Fourati <fourati.omar@gmail.com>
2025-01-08 21:02:59 +0100

    add assets and first logic for about me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'portfolio_resume';
+  skills = [
+    { name: 'HTML', icon: 'assets/html.png' },
+    { name: 'Tailwind CSS / CSS', icon: 'assets/css-3.png' },
+    { name: 'JavaScript', icon: 'assets/js.png' },
+    { name: 'TypeScript', icon: 'assets/typescript.png' },
+    { name: 'Angular', icon: 'assets/programing.png' },
+    { name: 'React', icon: 'assets/physics.png' },
+    { name: 'Node.js', icon: 'assets/nodejs.png' },
+    { name: 'Amazon Web Services', icon: 'assets/aws-brands-solid.svg' },
+    { name: 'REST API', icon: 'assets/api.png' },
+    { name: 'Agile Development', icon: 'assets/agile.png' },
+    { name: 'Python', icon: 'assets/python.png' },
+    { name: 'Git', icon: 'assets/social.png' },
+  ];
 }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"], // Ensure Tailwind scans your Angular files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
