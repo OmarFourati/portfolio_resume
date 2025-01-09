74dee46adb17762d53fc87df3363ba04d4dbcced
Omar Fourati <fourati.omar@gmail.com>
2025-01-09 20:54:43 +0100

    add about you text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent {
     { name: 'React', icon: 'assets/physics.png' },
     { name: 'Node.js', icon: 'assets/nodejs.png' },
     { name: 'Amazon Web Services', icon: 'assets/aws-brands-solid.svg' },
-    { name: 'REST API', icon: 'assets/api.png' },
+    { name: 'REST API / SQL /NO SQL', icon: 'assets/api.png' },
     { name: 'Agile Development', icon: 'assets/agile.png' },
     { name: 'Python', icon: 'assets/python.png' },
     { name: 'Git', icon: 'assets/social.png' },
